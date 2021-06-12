3f3203335c6b0f1a7c0567207b821ffefd481aec
abdulmumeen <abdulmumeen15500@gmail.com>
2021-06-12 09:06:25 +0100

    added a utility for string validation

---
+++ src/utils/validator.ts
@@ -0,0 +1,50 @@
+import * as Joi from "joi";
+
+export default class Validator {
+  static schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+    repeat_password: Joi.ref("password"),
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    }),
+  });
+
+  static async validateName(name: string): Promise<boolean> {
+    try {
+      const isValid = await this.schema.validate({ name: name });
+      if (!isValid) return false;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+  static async validatePassword(password: string): Promise<boolean> {
+    try {
+      const isValid = await this.schema.validate({ password: password });
+      if (!isValid) return false;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+  static async validateRepeatPassword(password: string): Promise<boolean> {
+    try {
+      const isValid = await this.schema.validate({ repeat_password: password });
+      if (!isValid) return false;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+  static async validateEmail(email: string): Promise<boolean> {
+    try {
+      const isValid = await this.schema.validate({ email: email });
+      if (!isValid) return false;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
