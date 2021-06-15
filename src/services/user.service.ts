8e6d8526b3002103be72e1a6ef4b558ec0f13c07
abdulmumeen <abdulmumeen15500@gmail.com>
2021-06-15 15:20:03 +0100

    fixed typo and added profile and credits update methods

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,8 @@
 import { IUserDoc, User } from "../models/user.model";
-import { IUserRegInfo as RegInfo } from "./constants";
+import {
+  IUserRegInfo as RegInfo,
+  IUserUpdateData as UpdateData,
+} from "./constants";
 
 export const createUser = async (info: RegInfo): Promise<boolean> => {
   try {
@@ -14,9 +17,9 @@ export const createUser = async (info: RegInfo): Promise<boolean> => {
   }
 };
 
-export const getUser = async (id: string): Promise<IUserDoc | null> => {
+export const getUser = async (userId: string): Promise<IUserDoc | null> => {
   try {
-    const user = await User.findOne({ _id: id });
+    const user = await User.findOne({ _id: userId });
     if (!user) return null;
     return user;
   } catch (error) {
@@ -24,9 +27,9 @@ export const getUser = async (id: string): Promise<IUserDoc | null> => {
   }
 };
 
-export const getVendor = async (id: string): Promise<IUserDoc | null> => {
+export const getVendor = async (userId: string): Promise<IUserDoc | null> => {
   try {
-    const vendor = await User.findOne({ _id: id, vendor: true });
+    const vendor = await User.findOne({ _id: userId, vendor: true });
     if (!vendor) return null;
     return vendor;
   } catch (error) {
@@ -57,5 +60,77 @@ export const fetchAllVendors = async (): Promise<Array<IUserDoc> | null> => {
     return vendors;
   } catch (error) {
     return null;
+  }
+};
+
+export const updateProfile = async (
+  userId: string,
+  details: UpdateData
+): Promise<boolean> => {
+  try {
+    const updated = await User.findOneAndUpdate(
+      { _id: userId },
+      { $set: { ...details } },
+      { new: true, runValidators: true }
+    );
+    if (!updated) return false;
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const updateCredit = async (
+  userId: string,
+  unit: number
+): Promise<boolean> => {
+  try {
+    // TODO: find a way to optimize this
+    // const updated = await User.findOneAndUpdate(
+    //   { _id: userId },
+    //   { $set: { credits: +(this.credits! + unit) } }
+    // );
+    // if (!updated) return false;
+    // return true;
+    const user = await User.findOne({ _id: userId });
+    if (!user) return false;
+    user.credits! += +unit;
+    const isSaved = await user.save();
+    if (!isSaved) return false;
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const assignStore = async (
+  userId: string,
+  storeId: string
+): Promise<boolean> => {
+  try {
+    const user = await User.findOne({ _id: userId, store: "" });
+    if (!user) return false;
+    user.store = storeId;
+    const isSaved = await user.save();
+    if (!isSaved) return false;
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const removeStore = async (
+  userId: string,
+  storeId: string
+): Promise<boolean> => {
+  try {
+    const user = await User.findOne({ _id: userId });
+    if (!user) return false;
+    user.store = "";
+    const isSaved = await user.save();
+    if (!isSaved) return false;
+    return true;
+  } catch (error) {
+    return false;
   }
 };
