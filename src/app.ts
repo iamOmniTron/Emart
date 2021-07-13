adc50f95e8ff05f95e6e225d31b2d61a706e32eb
abdulmumeen <abdulmumeen15500@gmail.com>
2021-07-13 13:44:19 +0100

    added bodyparser and fixed proper error handling

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,16 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import { SERVER_PORT as PORT } from "./config/config";
 require("./config/config/_dbinit")();
 import cors from "cors";
+import * as bodyParser from "body-parser";
 
 const app: Application = express();
 app.use(cors());
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   if (err) {
-    return res.status(400).json({
-      error: "internal server error",
+    return res.status(err.statusCode || 500).json({
+      error: err.message,
     });
   }
   next();
