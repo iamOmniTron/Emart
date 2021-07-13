249c9b8d5bcff47ba0be19b68039854d780aca44
abdulmumeen <abdulmumeen15500@gmail.com>
2021-07-13 13:45:16 +0100

    added coupon model

---
+++ src/models/coupons.model.ts
@@ -0,0 +1,38 @@
+import { model, Document, Schema, Types } from "mongoose";
+import { User } from "./user.model";
+
+const CouponSchema = new Schema({
+  code: {
+    type: String,
+    min: 6,
+  },
+  discount: {
+    type: Number,
+  },
+  used: {
+    type: Boolean,
+    default: false,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+  expiry: {
+    type: Date,
+  },
+  user: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+});
+
+export interface ICoupon extends Document {
+  code: string;
+  discount: number;
+  used?: boolean;
+  createdOn?: number;
+  expiry: number;
+  user: string;
+}
+
+export const Coupon = model<ICoupon>("Coupon", CouponSchema);
